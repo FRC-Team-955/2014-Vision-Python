809cd5d16d5f342e7923720ce92eba82b055b754
Merfoo <fauzimerfoo@gmail.com>
2014-01-16 16:49:54 -0800

    Filtered out noise, added detection for vert targets

--- src/vision.py
+++ src/vision.py
@@ -3,14 +3,14 @@ import math
 from rectangle import Rectangle
 import numpy as np
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 foundHotTarget = False
-viewAngleVert = 18.87 #37.5
+viewAngleVert = 28.5#18.87 #37.5
 resHalfY = 240
 horizTarget = Rectangle(0.0, 0.0, 23.5, 4.0)
 vertTarget = Rectangle(0.0, 0.0, 4.0, 32.0)
 
-cv2.namedWindow('hsv', cv2.WINDOW_NORMAL)
+cv2.namedWindow('color', cv2.WINDOW_NORMAL)
 cv2.namedWindow('filtered', cv2.WINDOW_NORMAL)
 
 print "vision name", __name__
@@ -20,16 +20,19 @@ def update():
 
     # Get color image from camera
     ret, img = camera.read() # img.shape 640x480 image
+    cv2.imshow("color", img)
+    cv2.waitKey(10)
 
     # Convert to hsv img
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    cv2.imshow("hsv", hsv)
-    cv2.waitKey(10)
 
     # Keep only green objects
-    lowerGreen = np.array([90, 0, 200])
-    upperGreen = np.array([120, 255,255])
+    lowerGreen = np.array([70, 100, 225])
+    upperGreen = np.array([110, 255,255])
     filteredGreen = cv2.inRange(hsv, lowerGreen, upperGreen)
+
+    # Filter out small objects
+    filteredGreen = cv2.morphologyEx(filteredGreen, cv2.MORPH_OPEN, np.ones((3, 3)))
     cv2.imshow("filtered", filteredGreen)
     cv2.waitKey(10)
 
@@ -44,13 +47,19 @@ def update():
         # If the width of the target is greater than its height then it's probably the hot target
         if target.width > target.height * 2.5:
             foundHotTarget = True
-            drawRect(hsv, target)
-            distance = computDistance(horizTarget.height, target.height)
-            print "Distance: ", round(distance), " Hot Target: ", foundHotTarget
-    cv2.imshow("hsv", hsv)
+            drawRect(img, target)
+            distance = computeDistance(horizTarget.height, target.height)
+            print "Distance: ", round(distance), ", Hot Target"
+
+        # If the height of the target is greater than the its width its probably a vert target
+        elif target.height > target.width * 7.5:
+            drawRect(img, target)
+            distance = computeDistance(vertTarget.height, target.height)
+            print "Distance: ", round(distance), ", Vert Target"
+    cv2.imshow("color", img)
     cv2.waitKey(10)
 
-def computDistance(realHeight, targetHeight):
+def computeDistance(realHeight, targetHeight):
     return ((realHeight / targetHeight) * resHalfY) / math.tan(viewAngleVert * math.pi / 180)
 
 def computeAngle(realHeight, targetHeight, distance):
