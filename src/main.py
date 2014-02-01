f9a67f149ef1613c9702e6dc622840f383a65568
Merfoo <fauzimerfoo@gmail.com>
2014-02-01 13:22:21 -0800

    Reading/Writing values to networktable, haven't thoroughly looked over it though.

--- src/main.py
+++ src/main.py
@@ -10,12 +10,27 @@ import time
 # if this script is run directly by python, then __name__ is '__main__'.  If it
 # is run because it is imported, then __name__ is the module name.
 
-tableName = "/955/"
-distanceName = ""
-
 if __name__ == '__main__': 
 
+	# Table name  = team number, used for ip address
+	tableName = "955"
 	table = NetworkTable(tableName)
+	tableDirectory = '/' + tableName + '/'
+
+	# Id's for reading data from networktable
+	isGoalHotId = "isGoalHot"
+	isGoalHorzId = "isGoalHorz"
+	goalDistanceId = "goalDistance"
+
+	# Id's for setting data to networktable
+	gyroAngleId = "gyroAngle"
+	gyroAngle = 0.0
+
 	while  True:
-    	vision.update()
+		gyroAngle = table.getValue(tableDirectory + gyroAngleId)
+    	vision.update(gyroAngle)
+    	table.setValue(tableDirectory + goalDistanceId, vision.getDistace())
+    	table.setValue(tableDirectory + isGoalHotId, vision.getFoundHotTarget())
+    	table.setValue(tableDirectory + isGoalHorzId, vision.getFoundHorzTarget())
+
     	time.sleep(1.0 / 4.0)
